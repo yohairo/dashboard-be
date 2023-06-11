b348b556f4d9125ac69cc7776403dcffdaa7e37a
Yohairo Vargas <yohairo.vargas@gmail.com>
2023-06-10 20:59:31 -0400

    setup documentation & route prefix

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,29 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.setGlobalPrefix('api/v1');
+  app.enableCors({
+    origin: "*",
+    credentials: true
+  })
+
+  // Documentation 
+  const options = new DocumentBuilder()
+    .setTitle('Dashboard API')
+    .setDescription('Dashboard project API')
+    .setVersion('1.0.0')
+    .addBearerAuth()
+    .build();
+
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('/api/docs', app, document, {
+    swaggerOptions : { filters : true }
+  })
+
+  await app.listen(process.env.PORT || 4000);
 }
 bootstrap();
