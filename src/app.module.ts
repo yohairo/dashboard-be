632271ee90b94f4c03af630f69dd6bb0745eaaed
Yohairo Vargas <yohairo.vargas@gmail.com>
2023-06-19 12:45:15 -0400

    fixing some errors

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,5 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { AuthModule } from './auth/auth.module';
-import { UserController } from './user/user.controller';
-import { UserService } from './user/user.service';
 import { UserModule } from './user/user.module';
-import { AuthController } from './auth/auth.controller';
-import { AuthService } from './auth/auth.service';
-import { databaseProviders } from './database/database.providers';
 import { ConfigModule } from '@nestjs/config';
 
 @Module({
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,15 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
-import { usersProviders } from './user.provicers';
+import { usersProviders } from './user.providers';
 import { DatabaseModule } from 'src/database/database.module';
-
 @Module({
-    imports: [DatabaseModule],
-    controllers: [UserController],
-    providers: [
-        UserService,
-        ...usersProviders
-    ]
+  imports: [DatabaseModule],
+  controllers: [UserController],
+  providers: [UserService, ...usersProviders],
 })
 export class UserModule {}
